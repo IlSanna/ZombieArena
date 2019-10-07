59e4af8cf533e487bae143b0f86d443307294407
IlSanna <IlSanna@users.noreply.github.com>
2019-10-07 16:59:11 +0200

    started definition for zombie class

---
+++ ZombieArena/Zombie.cpp
@@ -0,0 +1,10 @@
+#include "Zombie.h"
+#include "TextureHolder.h"
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+void Zombie::spawn(float startX, float startY, int type, int seed) {
+
+}
